29423c3b171c54f7a598349b9c828235e43a3b82
hyunjung <space90220@gmail.com>
2025-01-16 21:34:15 +0900

    Udemy : vue.js 강의 실습 (88 ~ 93)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+//import App from "./App.vue";
+//createApp(App).mount("#app");
+
+import UdSection07App from "./modules/udSection07/App.vue";
+import FriendContact from "./modules/udSection07/components/FriendContact.vue";
+const app = createApp(UdSection07App);
+app.component("friend-contact", FriendContact);
+app.mount("#app");
